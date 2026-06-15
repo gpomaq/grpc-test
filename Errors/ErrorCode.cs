b6f608066e0f4bdd7b01568f70fbe7e612136150
Gustavo Poma <gpoma@bancounion.com.bo>
2026-06-15 17:28:55 -0400

    feat: base response added

---
+++ Errors/ErrorCode.cs
@@ -0,0 +1,9 @@
+﻿namespace GrpcTest.Errors
+{
+    public struct ErrorCode
+    {
+        public const string SUC000 = nameof(SUC000);
+        public const string VAL001 = nameof(VAL001);
+        public const string ERR001 = nameof(ERR001);
+    }
+}
